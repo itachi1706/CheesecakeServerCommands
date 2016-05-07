cbce1d0348699b8120c245be0003d79d03775a71
itachi1706 <kennethsohyq@gmail.com>
2016-05-07 23:00:23 +0800

    Releasing Build 2.2

--- src/main/java/com/itachi1706/cheesecakeservercommands/reference/References.java
+++ src/main/java/com/itachi1706/cheesecakeservercommands/reference/References.java
@@ -8,7 +8,7 @@ public class References {
 
     public static final String MOD_ID = "CheesecakeServerCommands";
     public static final String MOD_NAME = "Cheesecake Server Commands";
-    public static final String VERSION = "1.7.10-2.1";
+    public static final String VERSION = "1.7.10-2.2";
     public static final String CLIENT_PROXY = "com.itachi1706.cheesecakeservercommands.proxy.ClientProxy";
     public static final String SERVER_PROXY = "com.itachi1706.cheesecakeservercommands.proxy.ServerProxy";
 }
