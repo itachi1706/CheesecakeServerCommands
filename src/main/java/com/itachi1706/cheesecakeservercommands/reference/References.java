cdd2ac7b590ade289fe3d13b836ec23692ce6dd3
itachi1706 <kennethsohyq@gmail.com>
2018-01-27 17:13:31 +0800

    Updated to version 2.5 (MC 1.12.2)

--- src/main/java/com/itachi1706/cheesecakeservercommands/reference/References.java
+++ src/main/java/com/itachi1706/cheesecakeservercommands/reference/References.java
@@ -8,7 +8,7 @@ public class References {
 
     public static final String MOD_ID = "cheesecakeservercommands";
     public static final String MOD_NAME = "Cheesecake Server Commands";
-    public static final String VERSION = "1.12.2-2.4";
+    public static final String VERSION = "1.12.2-2.5";
     public static final String CLIENT_PROXY = "com.itachi1706.cheesecakeservercommands.proxy.ClientProxy";
     public static final String SERVER_PROXY = "com.itachi1706.cheesecakeservercommands.proxy.ServerProxy";
 }
