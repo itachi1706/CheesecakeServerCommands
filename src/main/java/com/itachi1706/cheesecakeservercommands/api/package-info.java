0cd12edca1f2bd1f01d1c22972c750956ffb4ef0
itachi1706 <kennethsohyq@gmail.com>
2018-06-30 18:54:04 +0800

    Create a shell for a future API for the plugin (#12)

---
+++ src/main/java/com/itachi1706/cheesecakeservercommands/api/package-info.java
@@ -0,0 +1,5 @@
+@API(apiVersion = References.API_VERSION, owner=References.MOD_ID, provides = References.API_PROVIDES)
+package com.itachi1706.cheesecakeservercommands.api;
+
+import com.itachi1706.cheesecakeservercommands.reference.References;
+import net.minecraftforge.fml.common.API;
--- src/main/java/com/itachi1706/cheesecakeservercommands/reference/References.java
+++ src/main/java/com/itachi1706/cheesecakeservercommands/reference/References.java
@@ -7,8 +7,10 @@ package com.itachi1706.cheesecakeservercommands.reference;
 public class References {
 
     public static final String MOD_ID = "cheesecakeservercommands";
+    public static final String API_PROVIDES = "cheesecakeservercommands|API";
     public static final String MOD_NAME = "Cheesecake Server Commands";
     public static final String VERSION = "1.12.2-2.6";
+    public static final String API_VERSION = "1.12.2-0.1";
     public static final String CLIENT_PROXY = "com.itachi1706.cheesecakeservercommands.proxy.ClientProxy";
     public static final String SERVER_PROXY = "com.itachi1706.cheesecakeservercommands.proxy.ServerProxy";
 }
