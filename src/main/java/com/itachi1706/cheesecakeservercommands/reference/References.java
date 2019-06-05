5b11bd81dbbfc1166f3eb4e9aec56851a747e11d
itachi1706 <kennethsohyq@gmail.com>
2019-06-05 19:15:28 +0800

    Releasing 3.8 update to fix bugs

--- src/main/java/com/itachi1706/cheesecakeservercommands/reference/References.java
+++ src/main/java/com/itachi1706/cheesecakeservercommands/reference/References.java
@@ -10,7 +10,7 @@ public class References {
     public static final String MOD_ID_SHORT = "csc";
     public static final String API_PROVIDES = "cheesecakeservercommands|API";
     public static final String MOD_NAME = "Cheesecake Server Commands";
-    public static final String VERSION = "1.12.2-3.7";
+    public static final String VERSION = "1.12.2-3.8";
     public static final String API_VERSION = "1.12.2-0.1";
     public static final String CLIENT_PROXY = "com.itachi1706.cheesecakeservercommands.proxy.ClientProxy";
     public static final String SERVER_PROXY = "com.itachi1706.cheesecakeservercommands.proxy.ServerProxy";
