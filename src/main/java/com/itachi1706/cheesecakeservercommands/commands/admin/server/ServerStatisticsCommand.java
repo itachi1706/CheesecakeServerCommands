0ad2730c6a1a2490baf48cb5f63e47ceaca13ef1
Kenneth Soh <kenneth@itachi1706.com>
2022-07-01 14:34:54 +0800

    Finished up the ServerStatistics Command

--- src/main/java/com/itachi1706/cheesecakeservercommands/commands/admin/server/ServerStatisticsCommand.java
+++ src/main/java/com/itachi1706/cheesecakeservercommands/commands/admin/server/ServerStatisticsCommand.java
@@ -2,6 +2,7 @@ package com.itachi1706.cheesecakeservercommands.commands.admin.server;
 
 import com.itachi1706.cheesecakeservercommands.CheesecakeServerCommands;
 import com.itachi1706.cheesecakeservercommands.commands.BaseCommand;
+import com.itachi1706.cheesecakeservercommands.util.LogHelper;
 import com.itachi1706.cheesecakeservercommands.util.ServerUtil;
 import com.itachi1706.cheesecakeservercommands.util.TextUtil;
 import com.mojang.brigadier.Command;
@@ -9,10 +10,10 @@ import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import net.minecraft.ChatFormatting;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.Level;
 
 import javax.management.*;
-import java.text.NumberFormat;
 import java.util.stream.IntStream;
 
 public class ServerStatisticsCommand extends BaseCommand {
@@ -44,11 +45,9 @@ public class ServerStatisticsCommand extends BaseCommand {
 
         // RAM
         Runtime runtime = Runtime.getRuntime();
-        NumberFormat format = NumberFormat.getInstance();
 
         long maxMemory = runtime.maxMemory();
         long allocatedMemory = runtime.totalMemory();
-        long freeMemory = runtime.freeMemory();
         int processors = runtime.availableProcessors();
         long usedMemory = getUsedMemory(runtime);
         double percentageMemoryUsed = (usedMemory/(double) maxMemory) * 100;
@@ -59,28 +58,39 @@ public class ServerStatisticsCommand extends BaseCommand {
         sendSuccessMessage(sender, TextUtil.centerText("System Statistics"));
         sendSuccessMessage(sender, ChatFormatting.GOLD + TextUtil.generateChatBreaks());
         sendSuccessMessage(sender, String.format("CPU: %s (%d%%) [%s cores]", generatePercentageStep(percentageCPU), percentageCPU, ChatFormatting.LIGHT_PURPLE + "" + processors + ChatFormatting.RESET));
-        sendSuccessMessage(sender, String.format("Memory: %s " + ChatFormatting.GREEN + "%s" + ChatFormatting.RESET + "/" + ChatFormatting.RED + "%s" + ChatFormatting.RESET + " (%.0f%%)",
-                generatePercentageStep((int) percentageMemoryUsed), getReadableMemorySizeString(usedMemory), getReadableMemorySizeString(maxMemory), percentageMemoryUsed));
+        sendSuccessMessage(sender, String.format("Memory: %s %s%s%s/%s%s%s (%.0f%%)", generatePercentageStep((int) percentageMemoryUsed), ChatFormatting.GREEN,
+                getReadableMemorySizeString(usedMemory), ChatFormatting.RESET, ChatFormatting.RED, getReadableMemorySizeString(maxMemory), ChatFormatting.RESET, percentageMemoryUsed));
         sendSuccessMessage(sender, String.format("Allocated: %s (%.0f%%)", getReadableMemorySizeString(allocatedMemory), percentageMemoryAllocated));
         if (ServerUtil.getServerInstance().isDedicatedServer()) {
             // Get Players online
             int playersOnline = ServerUtil.getServerInstance().getPlayerCount();
             int maxPlayers = ServerUtil.getServerInstance().getMaxPlayers();
-            sendSuccessMessage(sender, String.format("Players: %s " + ChatFormatting.GREEN + "%s" + ChatFormatting.RESET + "/" + ChatFormatting.RED + "%s",
-                    generatePercentageStep((int) ((playersOnline/(double)maxPlayers) * 100)), playersOnline, maxPlayers));
+            sendSuccessMessage(sender, String.format("Players: %s %s%s%s/%s%s", generatePercentageStep((int) ((playersOnline/(double)maxPlayers) * 100)),
+                    ChatFormatting.GREEN, playersOnline, ChatFormatting.RESET, ChatFormatting.RED, maxPlayers));
         }
         sendSuccessMessage(sender, ChatFormatting.GOLD + TextUtil.generateChatBreaks());
         sendSuccessMessage(sender, TextUtil.centerText("TPS Info"));
         sendSuccessMessage(sender, ChatFormatting.GOLD + TextUtil.generateChatBreaks());
         MinecraftServer server = ServerUtil.getServerInstance();
         double meanTickTime = mean(server.tickTimes) * 1.0E-6D;
+        if (meanTickTime <= 0) {
+            meanTickTime = 0.00000000001D;
+        }
         double meanTPS = Math.min(1000.0/meanTickTime, 20);
-        sendSuccessMessage(sender, String.format("%s: " + ChatFormatting.LIGHT_PURPLE + "%.1f" + ChatFormatting.RESET + " (%.4fms)", "Overall", meanTPS, meanTickTime));
-        for (Level level : server.getAllLevels())
+        sendSuccessMessage(sender, String.format("Overall: %s%.1f%s (%.4fms)", ChatFormatting.LIGHT_PURPLE, meanTPS, ChatFormatting.RESET, meanTickTime));
+        for (ServerLevel level : server.getAllLevels())
         {
-            double worldTickTime = mean(server.getTickTime(level.dimension())) * 1.0E-6D;
+            long[] tickTimes = server.getTickTime(level.dimension());
+            if (tickTimes == null) {
+                LogHelper.warn("Skipping level {} as there are no tick times", level.dimension().location().toString());
+                continue;
+            }
+            double worldTickTime = mean(tickTimes) * 1.0E-6D;
+            if (worldTickTime <= 0) {
+                worldTickTime = 0.00000000001D;
+            }
             double worldTPS = Math.min(1000.0/worldTickTime, 20);
-            sendSuccessMessage(sender, String.format("%s: " + ChatFormatting.LIGHT_PURPLE + "%.1f" + ChatFormatting.RESET + " (%.4fms)", getDimensionPrefix(level), worldTPS, worldTickTime));
+            sendSuccessMessage(sender, String.format("%s: %s%.1f%s (%.4fms)", getDimensionPrefix(level), ChatFormatting.LIGHT_PURPLE, worldTPS, ChatFormatting.RESET, worldTickTime));
         }
 
         sendSuccessMessage(sender, ChatFormatting.GOLD + TextUtil.generateChatBreaks());
@@ -93,12 +103,8 @@ public class ServerStatisticsCommand extends BaseCommand {
 
     private static String getDimensionPrefix(Level level)
     {
-        // TODO: Test if this works as intended. If it does remove the old code
-        //DimensionType type = level.dimensionType();
-        String name = level.dimension().getRegistryName().toString();
-//        if (type == null)
-        return String.format("Dim " + ChatFormatting.AQUA + "%d" + ChatFormatting.RESET, name);
-        //else return String.format("Dim " + ChatFormatting.AQUA + "%d" + ChatFormatting.RESET + " (%s)", name, type.);
+        String name = level.dimension().location().toString();
+        return String.format("Dimension %s%s%s", ChatFormatting.AQUA, name, ChatFormatting.RESET);
     }
 
     private static long mean(long[] values)
@@ -116,8 +122,8 @@ public class ServerStatisticsCommand extends BaseCommand {
 
         StringBuilder sb = new StringBuilder();
         sb.append("[").append(ChatFormatting.AQUA);
-        IntStream.range(0, percentageStep).forEach((s) -> sb.append("="));
-        IntStream.range(0, 20 - percentageStep).forEach((s) -> sb.append(" "));
+        IntStream.range(0, percentageStep).forEach(s -> sb.append("="));
+        IntStream.range(0, 20 - percentageStep).forEach(s -> sb.append(" "));
         sb.append(ChatFormatting.RESET).append("]");
         return sb.toString();
     }
@@ -130,7 +136,6 @@ public class ServerStatisticsCommand extends BaseCommand {
             i++;
         } while (fileSizeInBytes > 1024);
 
-        //return Math.max(fileSizeInBytes, 0.1).toFixed(1) + byteUnits[i];
         return String.format("%.1f %s", Math.max(fileSizeInBytes, 0.1), byteUnits[i]);
     }
 }
