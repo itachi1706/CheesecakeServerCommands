dbfe0380b71e981704c32baf93d67c73f855a34d
Kenneth Soh <kenneth@itachi1706.com>
2022-07-09 21:56:06 +0800

    Set to 1.18 with a major version bump!

--- src/main/java/com/itachi1706/cheesecakeservercommands/reference/References.java
+++ src/main/java/com/itachi1706/cheesecakeservercommands/reference/References.java
@@ -13,5 +13,5 @@ public class References {
     public static final String MOD_ID = "cheesecakeservercommands";
     public static final String MOD_ID_SHORT = "csc";
     public static final String MOD_NAME = "Cheesecake Server Commands";
-    public static final String VERSION = "1.12.2-3.11";
+    public static final String VERSION = "1.18.2-4.0";
 }
